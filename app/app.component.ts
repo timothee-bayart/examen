71ec7e6957232f8efafc6697aece90b79fc40cf3
Timothée BAYART <timotheebayart@gmail.com>
2016-10-16 15:44:43 +0200

    #US4

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
+        <nav>
+            <a routerLink="/toDo">Tâches à faire</a>
+            <a routerLink="/toDoArchive">Archives</a>
+        </nav>
         <todo-page></todo-page>
     `
 })
